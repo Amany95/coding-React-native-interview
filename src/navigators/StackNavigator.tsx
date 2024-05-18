17b644946d56b760e1e38e13a19ba1be6eccea75
amany adel <36702658+Amany95@users.noreply.github.com>
2024-05-18 23:18:58 +0300

    make deep linking mytask://

--- src/navigators/StackNavigator.tsx
+++ src/navigators/StackNavigator.tsx
@@ -6,10 +6,23 @@ import {NavigationContainer} from '@react-navigation/native';
 import {MovieDetails} from '../containers/movies/screens/movieDetails/MovieDetails';
 
 const Stack = createStackNavigator();
-
+const linking = {
+  prefixes: ['mytask://'],
+  config: {
+    screens: {
+      BottomTabs: {
+        screens: {
+          Home: 'home',
+          Discover: 'discover',
+          Favourite: 'favourite',
+        },
+      },
+    },
+  },
+};
 function StackNavigator(): JSX.Element {
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <Stack.Navigator initialRouteName="BottomTabs">
         <Stack.Screen
           name="BottomTabs"
